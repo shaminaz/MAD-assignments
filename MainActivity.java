be70890683371cf3a0ed3753124ddb9697b58058
shaminaz <50742763+shaminaz@users.noreply.github.com>
2020-03-22 00:59:06 +0530

    Add files via upload
    android program to call an imageView in runtime

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,10 @@
-package com.example.assign1;
+package com.example.assign2;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Color;
-import android.graphics.Typeface;
+
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout lay;
@@ -13,28 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lay = findViewById(R.id.lay);
-        final TextView tv = new TextView(this);
-        lay.addView(tv);
-        new CountDownTimer(5000,3000)
-        {
-            public void onTick(long millisUntilFinished)
-            {
-                tv.setText(" Hello, my name is SHAMINAZ");
-                tv.setTypeface(tv.getTypeface(),Typeface.BOLD);
-                tv.setTextColor(Color.BLUE);
-                tv.setTextSize(85);
-            }
-
-            public void onFinish() {
-                tv.setTypeface(tv.getTypeface(),Typeface.ITALIC);
-                tv.setTextColor(Color.BLACK);
-
-            }
-        }.start();
-
-
-
+            lay = findViewById(R.id.lay);
+        final ImageView iv = new ImageView(this);
+        iv.setImageResource(R.drawable.ic_launcher_background);
+        lay.addView(iv);
     }
 }
-
